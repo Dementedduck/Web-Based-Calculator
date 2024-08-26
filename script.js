a7a1d4535dcd7789febe68d2a7245d7e5fd89237
Dementedduck <155681526+Dementedduck@users.noreply.github.com>
2024-08-26 20:02:45 +0100

    Change divs to buttons Add basic input functionality Add display population Make calculator a set size

--- script.js
+++ script.js
@@ -1,44 +1,66 @@
-let firstNum = undefined;
-let secondNum = undefined;
-let operator = "+";
+const buttons = document.querySelector(".buttons");
+const inputs = buttons.querySelectorAll(".numbers");
+let firstNum = "";
+let secondNum = "";
+let operator = undefined;
+let display = document.querySelector("#calcDisplay");
+let displayValue = "000000000";
 
-
-
-function add (a, b) {
-    return (a + b)
+function populateDisplay(nums) {
+  let dig = document.createElement("div");
+  display.insertAdjacentElement("beforeend", dig);
+  dig.textContent = nums;
+  dig.style.fontSize = "30px";
+  dig.className = "digits";
 }
 
-function subtract (a,b) {
-    return (a - b)
+populateDisplay(displayValue);
+
+inputs.forEach((input) => {
+  input.addEventListener("click", function () {
+    let digit = display.querySelectorAll(".digits");
+    let displayLength = digit.length;
+
+    if (displayLength <= 9) {
+      let value = (firstNum += input.innerHTML);
+      let dig = display.querySelector("div");
+      dig.textContent = "";
+      populateDisplay(value);
+      firstNum = "";
+    }
+  });
+});
+
+function add(a, b) {
+  return a + b;
 }
 
-function multiply (a,b) {
-    return (a * b)
+function subtract(a, b) {
+  return a - b;
 }
 
-function divide (a,b) {
-    return (a / b)
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
 }
 
 function operate(first, second, operate) {
-if (operate == "+") {
-    let result = add (first, second); 
-    console.log(result) 
-}
-else if (operate == "-") {
+  if (operate == "+") {
+    let result = add(first, second);
+    console.log(result);
+  } else if (operate == "-") {
     let result = subtract(first, second);
-    console.log(result)
-}
-else if (operate == "*") {
-    let result =  multiply(first, second);
-    console.log(result)
-}
-else if (operate == "/") {
+    console.log(result);
+  } else if (operate == "*") {
+    let result = multiply(first, second);
+    console.log(result);
+  } else if (operate == "/") {
     let result = divide(first, second);
-    console.log(result)
-} else {
-    (console.log("ERRR"))
+    console.log(result);
+  } else {
+    console.log("ERRR");
+  }
 }
-
-}
-
